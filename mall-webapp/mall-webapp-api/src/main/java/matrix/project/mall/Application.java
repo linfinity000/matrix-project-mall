af82655312bfdc7e16e1042aae5f56f2a1f47881
vwkit <admin@vwkit.com>
2020-02-27 17:24:49 +0800

    新增骨架

---
+++ mall-webapp/mall-webapp-api/src/main/java/matrix/project/mall/Application.java
@@ -0,0 +1,17 @@
+package matrix.project.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangcheng
+ * @date 2020-02-27
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
