d2b06834e8d6e9d44ea219dbd813223dd5956a94
vwkit <admin@vwkit.com>
2020-03-01 17:18:25 +0800

    商品骨架

---
+++ mall-common/src/main/java/matrix/project/mall/dto/GoodsDto.java
@@ -0,0 +1,42 @@
+package matrix.project.mall.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import matrix.module.based.serializer.DateTimeSerializer;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author wangcheng
+ * @date 2020-03-01
+ */
+@Data
+@Accessors(chain = true)
+public class GoodsDto implements Serializable {
+
+    private String goodsId;
+
+    private String atomsGoodsId;
+
+    private String atomsGoodsName;
+
+    private BigDecimal originPrice;
+
+    private BigDecimal salePrice;
+
+    private Integer stock;
+
+    private String imageUrl;
+
+    @JsonSerialize(using = DateTimeSerializer.class)
+    private Date createTime;
+
+    @JsonSerialize(using = DateTimeSerializer.class)
+    private Date updateTime;
+
+    private Integer status;
+
+}
---
+++ mall-webapp/mall-webapp-admin/src/main/java/matrix/project/mall/controller/GoodsController.java
@@ -0,0 +1,15 @@
+package matrix.project.mall.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangcheng
+ * @date 2020-03-01
+ */
+@RestController
+@RequestMapping("/api/goods")
+public class GoodsController {
+
+
+}
