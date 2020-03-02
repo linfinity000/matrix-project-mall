5edac0b48fd8211b9f90c13e1b62427c7ca796ad
vwkit <admin@vwkit.com>
2020-03-02 13:25:55 +0800

    新增购物车表

--- mall-common/src/main/resources/db/master/V2020090201__init.sql
+++ mall-common/src/main/resources/db/master/V2020090201__init.sql
@@ -6,6 +6,6 @@ CREATE TABLE user_cart
     GOODS_COUNT INT(10)      NOT NULL COMMENT '商品数量',
     CREATE_TIME DATETIME     NOT NULL COMMENT '创建时间',
     UPDATE_TIME DATETIME     NOT NULL COMMENT '更新时间',
-    STATUS      INT(1)       NOT NULL COMMENT '状态(1:上架,-1:删除)',
+    STATUS      INT(1)       NOT NULL COMMENT '状态(1:正常,-1:删除)',
     PRIMARY KEY (ID)
 );
