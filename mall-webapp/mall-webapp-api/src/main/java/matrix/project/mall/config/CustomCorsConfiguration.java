17446c6eb9f13a4416ac1e45a6dc8f387fc024fe
vwkit <admin@vwkit.com>
2020-03-28 13:28:11 +0800

    新增跨域配置

---
+++ mall-webapp/mall-webapp-api/src/main/java/matrix/project/mall/config/CustomCorsConfiguration.java
@@ -0,0 +1,33 @@
+package matrix.project.mall.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 设置支持跨域
+ *
+ * @author wangcheng
+ * @date 2020-03-28
+ */
+@Configuration
+public class CustomCorsConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
